448beee6073e0d9ff43ef60938f01ae45aaa597d
Alexey Kharitonov <senator220291@gmail.com>
2021-01-12 12:49:45 +0300

    update project

--- core/src/main/java/com/epam/core/Util.java
+++ core/src/main/java/com/epam/core/Util.java
@@ -2,17 +2,14 @@ package com.epam.core;
 
 import com.kharitonov.utils.StringUtils;
 
+import java.util.Arrays;
+
 public class Util {
 
     private Util() {
     }
 
     public static boolean isAllPositiveNumbers(String... str) {
-        for (String s : str) {
-            if (!StringUtils.isPositiveNumber(s)) {
-                return false;
-            }
-        }
-        return true;
+       return Arrays.stream(str).allMatch(StringUtils::isPositiveNumber);
     }
 }
